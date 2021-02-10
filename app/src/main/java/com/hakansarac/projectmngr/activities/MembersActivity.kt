e91048ee8dc54480c5e6b6f34cc2bc1955aab0c2
hakansarac <hakan.sarac95@gmail.com>
2021-02-10 22:07:41 +0300

    members activity created

---
+++ app/src/main/java/com/hakansarac/projectmngr/activities/MembersActivity.kt
@@ -0,0 +1,12 @@
+package com.hakansarac.projectmngr.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hakansarac.projectmngr.R
+
+class MembersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_members)
+    }
+}
--- app/src/main/java/com/hakansarac/projectmngr/activities/TaskListActivity.kt
+++ app/src/main/java/com/hakansarac/projectmngr/activities/TaskListActivity.kt
@@ -1,7 +1,10 @@
 package com.hakansarac.projectmngr.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hakansarac.projectmngr.R
 import com.hakansarac.projectmngr.adapters.TaskListItemsAdapter
@@ -27,6 +30,21 @@ class TaskListActivity : BaseActivity() {
 
         showProgressDialog(resources.getString(R.string.please_wait))
         FirestoreClass().getBoardDetails(this,boardDocumentId)  //call the getBoardDetails with clicked board
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_members, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.actionMembers -> {
+                val intent = Intent(this,MembersActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /**
